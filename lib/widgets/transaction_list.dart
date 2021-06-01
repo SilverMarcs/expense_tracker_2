0499ef977555285fefc763d282936e2da79ebced
Zabir Raihan <zabirraihan6@gmail.com>
2021-06-02 04:55:39 +0600

    1st

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -10,60 +10,59 @@ class TransactionList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 300, //need to manually define height because otherwise scrollview wont know how much exactly to scroll. now the column knows not to take size depending on its chilren but rather will take depending on the heightwe defined
-      child: SingleChildScrollView(  //will scroll within those 300px only
-        child: Column(
-          children: transactions.map((tx) { //tx is a random word for now. this is why use this.item
-            return Card(
-              elevation: 5,
-              child: Row(
-                children: <Widget>[
-                  Container(
-                    margin: EdgeInsets.symmetric(
-                      vertical: 10,
-                      horizontal: 15,
+      height: 300,
+      child: ListView(  //a column with scrollview 
+        children: transactions.map((tx) {
+          //tx is a random word for now. this is why use this.item
+          return Card(
+            elevation: 5,
+            child: Row(
+              children: <Widget>[
+                Container(
+                  margin: EdgeInsets.symmetric(
+                    vertical: 10,
+                    horizontal: 15,
+                  ),
+                  decoration: BoxDecoration(
+                    border: Border.all(
+                      color: Colors.purple,
+                      width: 2,
                     ),
-                    decoration: BoxDecoration(
-                      border: Border.all(
-                        color: Colors.purple,
-                        width: 2,
-                      ),
+                  ),
+                  padding: EdgeInsets.all(10),
+                  child: Text(
+                    "\$${tx.amount}", //string interpolatin instead of tx.amount.toString()
+                    style: TextStyle(
+                      fontWeight: FontWeight
+                          .bold, //bold is a static label so we need notinstantiate them
+                      fontSize: 20,
+                      color: Colors.purple,
                     ),
-                    padding: EdgeInsets.all(10),
-                    child: Text(
-                      "\$${tx.amount}", //string interpolatin instead of tx.amount.toString()
+                  ),
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(
+                      tx.title,
                       style: TextStyle(
-                        fontWeight: FontWeight
-                            .bold, //bold is a static label so we need notinstantiate them
-                        fontSize: 20,
-                        color: Colors.purple,
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
                       ),
                     ),
-                  ),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      Text(
-                        tx.title,
-                        style: TextStyle(
-                          fontSize: 16,
-                          fontWeight: FontWeight.bold,
-                        ),
+                    Text(
+                      DateFormat.yMMMd()
+                          .format(tx.date), //DFateFormat(yyyyMMdd) also works
+                      style: TextStyle(
+                        color: Colors.grey,
                       ),
-                      Text(
-                        DateFormat.yMMMd()
-                            .format(tx.date), //DFateFormat(yyyyMMdd) also works
-                        style: TextStyle(
-                          color: Colors.grey,
-                        ),
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-            );
-          }).toList(), //transforming list of objects to list of wdiegts  //map takes a fucntion that gets executed on every element on the list
-        ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          );
+        }).toList(), //transforming list of objects to list of wdiegts  //map takes a fucntion that gets executed on every element on the list
       ),
     );
   }
